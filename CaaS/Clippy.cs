46c2368cd2f53989ce833bf49d503dcb06af5769
dither <ditheredlights@gmail.com>
2023-05-20 00:53:15 -0400

    populate repo

---
+++ CaaS/Clippy.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace CaaS
+{
+    // clippy renderer
+    // written for kasumi by Toxoid49b
+    public static class Clippy
+    {
+        static Font clippyFont = new Font("Tahoma", 8.0f);
+        public static byte[] DrawClippy(string input)
+        {
+            // Create temporary bitmap to measure input text size
+            using (Bitmap tmpBmp = new Bitmap(1, 1, PixelFormat.Format32bppArgb))
+            using (Graphics tmpGx = Graphics.FromImage(tmpBmp))
+            {
+                // Windows fucks up trying to measure the text height unless you specify AA
+                tmpGx.TextRenderingHint = TextRenderingHint.AntiAlias;
+                SizeF textSize = tmpGx.MeasureString(input, clippyFont, 180, StringFormat.GenericTypographic);
+                int textHeight = (int)Math.Ceiling(textSize.Height);
+
+                // Create a new bitmap for the final image
+                using (Bitmap imgBmp = new Bitmap(200, textHeight + 16 + 96, PixelFormat.Format32bppArgb))
+                using (Graphics imgGx = Graphics.FromImage(imgBmp))
+                {
+                    imgGx.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
+
+                    // Create the various pens and brushes used to draw the image
+                    using (Pen borderPen = new Pen(Brushes.Black))
+                    using (SolidBrush rectBrush = new SolidBrush(Color.FromArgb(0xFF, 0xFF, 0xCC)))
+                    {
+                        borderPen.Width = 1.0f;
+
+                        // Fill in the background and text box parts
+                        //imgGx.FillRectangle(backBrush, new Rectangle(0, 0, imgBmp.Width, imgBmp.Height));
+                        imgGx.FillRectangle(rectBrush, new Rectangle(0, 8, 200, textHeight));
+                        imgGx.DrawRectangle(borderPen, new Rectangle(0, 7, 199, textHeight + 1));
+
+                        // Draw the input text
+                        imgGx.DrawString(input, clippyFont, Brushes.Black, new RectangleF(10, 8, 180, textHeight), StringFormat.GenericTypographic);
+
+                        // Draw the static image resources
+                        imgGx.DrawImageUnscaled(Properties.Resources.clippytop, 0, 0);
+                        imgGx.DrawImageUnscaled(Properties.Resources.clippybottom, 0, textHeight + 8);
+                        imgGx.DrawImageUnscaled(Properties.Resources.clippy, 32, imgBmp.Height - 86);
+
+                        // scale up the image
+                        const int scalex = 4;
+                        using (Bitmap scale = new Bitmap(imgBmp.Width * scalex, imgBmp.Height * scalex))
+                        using (Graphics scalectx = Graphics.FromImage(scale))
+                        {
+                            scalectx.SmoothingMode = SmoothingMode.None;
+                            scalectx.InterpolationMode = InterpolationMode.NearestNeighbor;
+                            scalectx.DrawImage(imgBmp, new Rectangle(0, 0, imgBmp.Width * scalex, imgBmp.Height * scalex));
+
+                            // Return final image (don't forget to dispose it when you're done)
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                scale.Save(ms, ImageFormat.Png);
+                                ms.Seek(0, SeekOrigin.Begin);
+                                return ms.ToArray();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# caas
-win32 microservice used by my discord bot
+# CaaS ("Clippy as a Service")
+
+![clippy](./239db45d4230b918b48d44d75de397af.png)
+
+win32 microservice used by my chat bot. targets .NET FX 4.0.
+
+said bot was originally written in C#, but completely rewritten in typescript later. some features i previously had or want to add are annoying to port/implement on node+linux.
+
+all this application does is host a tiny HTTP server with a few endpoints for generating content. all endpoints take POST requests with plaintext bodies. it does a lot of things synchronously, so i can assure you it absolutely will not scale well.
+
+the only thing currently really of note for most people is the clippy generator, which was written by my good friend Toxoid49b out of pity back in the day because mine was so bad.
+
+current features:
+- `/clippy` clippy image generator
+- `/dectalk` say.exe wrapper (you need to have say.exe in `(app root)/bin/say.exe`)
